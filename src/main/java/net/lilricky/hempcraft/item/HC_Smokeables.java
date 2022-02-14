c89d8cfa34a62bc037373dd72f34d1d3f3e779bc
Richard "Ricky" LaFleur <richardrickylafleur@gmail.com>
2022-02-14 13:39:29 -0400

    Added notes
    Tested, everything seams to have the right  effects.

--- src/main/java/net/lilricky/hempcraft/item/HC_Smokeables.java
+++ src/main/java/net/lilricky/hempcraft/item/HC_Smokeables.java
@@ -1,3 +1,7 @@
+// All the logic behind the smokable objects.
+//      This file deals with the extensive logic behind how the effects work with the different objects (joints, cones, and blunts).
+//      Since the Instant Health Status Effect works on a different scale (very low values for healing vs. high values for duration).
+
 package net.lilricky.hempcraft.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -12,10 +16,10 @@ import net.minecraft.util.UseAction;
 
 public class HC_Smokeables 
 {
-
     private String Name;
     private FoodComponent fc;
 
+    // Effects set by strain, and grade.
     private StatusEffect Effect1;
     private int effect1_duration = -1;
     
@@ -28,19 +32,14 @@ public class HC_Smokeables
     private StatusEffect Effect4;
     private int effect4_duration = -1;
 
-
-    static private StatusEffectInstance Heal1 = new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1);
-    static private StatusEffectInstance Slow = new StatusEffectInstance(StatusEffects.SLOWNESS, 15);
-
-
-    // Builds All Smokeable Items from Strain List.
+    // Constructor for the strain
     public HC_Smokeables(String name)
     {
         Name = name;
-
-        HempCraft.LOGGER.info("Creating New Smokable Objects: " + Name);
+        HempCraft.LOGGER.debug("Creating New Smokable Objects: " + Name);
     }
 
+    // Grade A has 0 variation. All are the same (because its shit weed).
     public void A()
     {
         Effect1 = StatusEffects.INSTANT_HEALTH;
@@ -51,25 +50,28 @@ public class HC_Smokeables
         BuildAll();
     }
 
+    // Grade AA has 2 effects, but would still recommend using Instant Health.
     public void AA(StatusEffect effect1, StatusEffect effect2)
     {
         Effect1 = effect1;
         Effect2 = effect2;
 
-        HempCraft.LOGGER.info("Creating Strain AA");
+        HempCraft.LOGGER.debug("Creating Strain AA");
         BuildAll();
     }
 
+    // Grade AAA has of course 3 effects.
     public void AAA(StatusEffect effect1, StatusEffect effect2, StatusEffect effect3)
     {
         Effect1 = effect1;
         Effect2 = effect2;
         Effect3 = effect3;
 
-        HempCraft.LOGGER.info("Creating Strain AAA");
+        HempCraft.LOGGER.debug("Creating Strain AAA");
         BuildAll();
     }
 
+    // Builds all Smokable Objects
     private void BuildAll()
     {
         Build("joint");
@@ -77,9 +79,10 @@ public class HC_Smokeables
         Build("blunt");
     }
 
+    // Builds the objects with a stupid amount of logic.
     private void Build(String ID)
     {
-        HempCraft.LOGGER.info("Building: " + ID);
+        HempCraft.LOGGER.debug("Building: " + ID);
 
         // Setting the Duration of all effects for proper smokable object
         if(ID == "joint")
@@ -106,10 +109,10 @@ public class HC_Smokeables
             effect4_duration = 135*20;
         }
 
-        // Set duration of effect for instant health
+        // Overriding Effect Duration if an effect is INSTANT_HEALTH
         if(Effect1 == StatusEffects.INSTANT_HEALTH)
         {
-            HempCraft.LOGGER.info("Effect 1 set to INSTANT_HEALTH");
+            HempCraft.LOGGER.debug("Effect 1 set to INSTANT_HEALTH");
 
             if(ID == "joint") { effect1_duration = 1; }
             if(ID == "cone") { effect1_duration = 2; }
@@ -118,7 +121,7 @@ public class HC_Smokeables
 
         if(Effect2 == StatusEffects.INSTANT_HEALTH)
         {
-            HempCraft.LOGGER.info("Effect 2 set to INSTANT_HEALTH");
+            HempCraft.LOGGER.debug("Effect 2 set to INSTANT_HEALTH");
 
             if(ID == "joint") { effect2_duration = 1; }
             if(ID == "cone") { effect2_duration = 2; }
@@ -127,7 +130,7 @@ public class HC_Smokeables
 
         if(Effect3 == StatusEffects.INSTANT_HEALTH)
         {
-            HempCraft.LOGGER.info("Effect 3 set to INSTANT_HEALTH");
+            HempCraft.LOGGER.debug("Effect 3 set to INSTANT_HEALTH");
 
             if(ID == "joint") { effect3_duration = 1; }
             if(ID == "cone") { effect3_duration = 2; }
@@ -136,17 +139,18 @@ public class HC_Smokeables
 
         if(Effect4 == StatusEffects.INSTANT_HEALTH)
         {
-            HempCraft.LOGGER.info("Effect 4 set to INSTANT_HEALTH");
+            HempCraft.LOGGER.debug("Effect 4 set to INSTANT_HEALTH");
 
             if(ID == "joint") { effect4_duration = 1; }
             if(ID == "cone") { effect4_duration = 2; }
             if(ID == "blunt") { effect4_duration = 3; }
         }
 
-        // TODO: Optimize the food component builder
+        // Food Builder Sequence
+        // Finds how many effects a strain has, and initiates the builder dependant on effects.
         if(Effect4 != null)
         {
-            HempCraft.LOGGER.info("Effect 4 Exists");
+            HempCraft.LOGGER.debug("Effect 4 Exists");
 
             fc = new FoodComponent.Builder()
             .hunger(0)
@@ -160,7 +164,7 @@ public class HC_Smokeables
         }
         else if(Effect3 != null)
         {
-            HempCraft.LOGGER.info("Effect 3 Exists");
+            HempCraft.LOGGER.debug("Effect 3 Exists");
 
             fc = new FoodComponent.Builder()
             .hunger(0)
@@ -173,7 +177,7 @@ public class HC_Smokeables
         }
         else if(Effect2 != null)
         {
-            HempCraft.LOGGER.info("Effect 2 Exists");
+            HempCraft.LOGGER.debug("Effect 2 Exists");
             
             fc = new FoodComponent.Builder()
             .hunger(0)
@@ -185,7 +189,7 @@ public class HC_Smokeables
         }
         else if(Effect1 != null)
         {
-            HempCraft.LOGGER.info("Effect 1 Exists");
+            HempCraft.LOGGER.debug("Effect 1 Exists");
 
             fc = new FoodComponent.Builder()
             .hunger(0)
@@ -195,7 +199,7 @@ public class HC_Smokeables
             .build();
         }
 
-
+        // Registers Item generated into the registry.
         ModItems.registerItem(ID + "/" + Name, new Item(new FabricItemSettings().food(fc).group(HempCraft.HC_ITEM_GROUP))
         {
             @Override
