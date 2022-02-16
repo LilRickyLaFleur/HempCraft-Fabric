3bdfa1cc0bf49d657706282d32b1c6bc88ed4c60
Richard "Ricky" LaFleur <richardrickylafleur@gmail.com>
2022-02-15 22:39:07 -0400

    Unable to render plant.
    Its not the image, the minecraft poppy texture does the same thing.
    
    Finished work for tonight, will find solution tomororw.

--- src/main/java/net/lilricky/hempcraft/block/ModBlocks.java
+++ src/main/java/net/lilricky/hempcraft/block/ModBlocks.java
@@ -3,18 +3,22 @@ package net.lilricky.hempcraft.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.lilricky.hempcraft.HempCraft;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks 
 {
 
-    public static final Block TEST_PLANT = registerBlock("plant", new Block(FabricBlockSettings.of(Material.PLANT).strength(6f).requiresTool()), HempCraft.HC_ITEM_GROUP);
+    public static final Block TEST_PLANT = registerBlock("plant", new Block(FabricBlockSettings.of(Material.PLANT).nonOpaque().noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP)), HempCraft.HC_ITEM_GROUP);
+
+    
 
     private static Block registerBlock(String name, Block block, ItemGroup group)
     {
